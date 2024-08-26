40a01ffba92969a842b5d1ebacb6614ff4ca40de
MakinaTeam <158822378+MakinaTeam@users.noreply.github.com>
2024-08-26 11:41:18 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -974,7 +974,7 @@ function Library:CreateWindow(a)
 	Credit.BorderSizePixel = 0
 	Credit.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	Credit.Size = UDim2.new(0, 217, 0, 43)
-	Credit.Position = UDim2.new(0.38737, 0, 0.68605, 0)
+	Credit.Position = UDim2.new(0.38737, 0, 0.05667, 0)
 	Credit.BorderColor3 = Color3.fromRGB(0, 0, 0)
 	Credit.Name = "Credit"
 
