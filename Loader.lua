534cf2890024934b38a3f3df72299c037eb4a71f
MakinaTeam <158822378+MakinaTeam@users.noreply.github.com>
2024-12-03 19:04:17 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,17 @@
 print("Kaitun Loader | Ver 1")
 game.StarterGui:SetCore("SendNotification", {
     Title = "Danny Hub | Kaitun", 
-    Text = "".._G.Doing(''),
+    Text = ""..DoingText(''),
     --Icon = "",
-    Duration = 99999999999
+    Duration = 99999999
 })
+spawn(function()
+    while wait() do
+        pcall(function()
+            DoingText = _G.Doing
+        end)
+    end
+end)
 if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
 repeat wait() until game.Players
 repeat wait() until game.Players.LocalPlayer
