49c4536a4118567fe48df5344195d60949e3034c
MakinaTeam <158822378+MakinaTeam@users.noreply.github.com>
2024-12-03 18:03:06 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -981,77 +981,228 @@ function Warp(p)
     tweenfunc:Stop()
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = p
 end
-function TP(p)
-    if W3 then
-        if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(-12476.080078125, 374.9144592285156, -7566.93701171875)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(-12476.080078125, 374.9144592285156, -7566.93701171875)) --Big Mansion
-        elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(5742.9169921875, 610.7672729492188, -268.2395935058594)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(5742.9169921875, 610.7672729492188, -268.2395935058594)) --Hydra Island
-        elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(-5076.99853515625, 314.8587341308594, -3151.824951171875)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(-5076.99853515625, 314.8587341308594, -3151.824951171875)) --Castle on the Sea
-        elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(-11993.580078125, 331.8077087402344, -8844.1826171875)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(-11993.580078125, 331.8077087402344, -8844.1826171875)) --Canvender T
-        elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(5314.58203125, 22.53643226623535, -125.94227600097656)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(5314.58203125, 22.53643226623535, -125.94227600097656)) --Canvender H
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.X, 60, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Z)
-        elseif (Vector3.new(-1990.672607421875, 4532.998046875, -14973.6748046875)-p.Position).Magnitude < 1500 and (Vector3.new(-1990.672607421875, 4532.998046875, -14973.6748046875)-game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude > 1500 then --Cake Prince
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2149.209228515625, 80.00882720947266, -12402.974609375)
-        elseif (Vector3.new(28609.388671875, 14896.8505859375, 105.2657241821289)-p.Position).Magnitude > 1500 and (Vector3.new(28609.388671875, 14896.8505859375, 105.2657241821289)-game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude < 1500 then --temple
-            TP(CFrame.new(28609.388671875, 14896.8505859375, 105.2657241821289))
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("RaceV4Progress","Check")
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("RaceV4Progress","TeleportBack")
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("SetLastSpawnPoint","GreatTree")
-        end
-    elseif W2 then
-        if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(923.2125244140625, 125.40035247802734, 32852.83203125)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(923.2125244140625, 125.40035247802734, 32852.83203125)) --Ghost Ship
-        elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(-286.6488037109375, 306.130615234375, 596.5967407226562)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(-286.6488037109375, 306.130615234375, 596.5967407226562)) --ManSion
-        elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(2283.7080078125, 15.277364730834961, 905.8712158203125)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(2283.7080078125, 15.277364730834961, 905.8712158203125)) --Sawn
-        elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(-6505.7294921875, 83.31233978271484, -130.24234008789062)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(-6505.7294921875, 83.31233978271484, -130.24234008789062)) --Out Ghost Ship
-        end
-    elseif W1 then
-        if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(3864.6884765625, 5.716391563415527, -1926.214111328125)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(3864.6884765625, 5.716391563415527, -1926.214111328125)) --Gate
-        elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(61163.8515625, 5.650546073913574, 1819.7841796875)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(61163.8515625, 5.650546073913574, 1819.7841796875)) --Under Water
-        elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(-7894.61767578125, 5545.8349609375, -380.29119873046875)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(-7894.61767578125, 5545.8349609375, -380.29119873046875)) --Sky2
-        elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position-p.Position).Magnitude > (Vector3.new(-4607.82275, 872.54248, -1667.55688)-p.Position).Magnitude then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(-4607.82275, 872.54248, -1667.55688)) --Sky1
-        end
-    end
-        --local Distance = (p.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
-    	--game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(Distance/300, Enum.EasingStyle.Linear),{CFrame = p}):Play()
-    	--game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.X, p.Y, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Z)
-    local RealtargetPos = {p}
-	local targetPos = RealtargetPos[1]
-	local RealTarget
-	if type(targetPos) == "vector" then
-		RealTarget = CFrame.new(targetPos)
-	elseif type(targetPos) == "userdata" then
-		RealTarget = targetPos
-	elseif type(targetPos) == "number" then
-		RealTarget = CFrame.new(unpack(RealtargetPos))
-	end
 
-	if game.Players.LocalPlayer.Character:WaitForChild("Humanoid").Health == 0 then if tween then tween:Cancel() end repeat wait() until game.Players.LocalPlayer.Character:WaitForChild("Humanoid").Health > 0; wait(0.2) end
-
-	local tweenfunc = {}
-	local Distance = (RealTarget.Position - game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position).Magnitude
-	if Distance < 1000 then
-		Speed = 315
-	elseif Distance >= 1000 then
-		Speed = 300
+if World3 then
+	distbyp = 5000;
+	island = {
+		["Port Town"] = CFrame.new( -290.7376708984375, 6.729952812194824, 5343.5537109375),
+		["Hydra Island"] = CFrame.new(5749.7861328125 + 50 , 611.9736938476562, -276.2497863769531),
+		Mansion = CFrame.new( -12471.169921875 + 50 , 374.94024658203, -7551.677734375),
+		["Castle On The Sea"] = CFrame.new( -5085.23681640625 + 50 , 316.5072021484375, -3156.202880859375),
+		["Haunted Island"] = CFrame.new( -9547.5703125, 141.0137481689453, 5535.16162109375),
+		["Great Tree"] = CFrame.new(2681.2736816406, 1682.8092041016, -7190.9853515625),
+		["Candy Island"] = CFrame.new( -1106.076416015625, 13.016114234924316, -14231.9990234375),
+		["Cake Island"] = CFrame.new( -1903.6856689453125, 36.70722579956055, -11857.265625),
+		["Loaf Island"] = CFrame.new( -889.8325805664062, 64.72842407226562, -10895.8876953125),
+		["Peanut Island"] = CFrame.new( -1943.59716796875, 37.012996673583984, -10288.01171875),
+		["Cocoa Island"] = CFrame.new(147.35205078125, 23.642955780029297, -12030.5498046875),
+		["Tiki Outpost"] = CFrame.new( -16234, 9, 416)
+	};
+elseif World2 then
+	distbyp = 3500;
+	island = {
+		A = CFrame.new(753.14288330078, 408.23559570313, -5274.6147460938),
+		B = CFrame.new( -5622.033203125, 492.19604492188, -781.78552246094),
+		C = CFrame.new( -11.311455726624, 29.276733398438, 2771.5224609375),
+		D = CFrame.new( -2448.5300292969, 73.016105651855, -3210.6306152344),
+		E = CFrame.new( -380.47927856445, 77.220390319824, 255.82550048828),
+		F = CFrame.new( -3032.7641601563, 317.89672851563, -10075.373046875),
+		G = CFrame.new(6148.4116210938, 294.38687133789, -6741.1166992188),
+		H = CFrame.new(923.40197753906, 125.05712890625, 32885.875),
+		I = CFrame.new( -6127.654296875, 15.951762199402, -5040.2861328125)
+	};
+elseif World1 then
+	distbyp = 1500;
+	island = {
+		A = CFrame.new(979.79895019531, 16.516613006592, 1429.0466308594),
+		B = CFrame.new( -2566.4296875, 6.8556680679321, 2045.2561035156),
+		C = CFrame.new(944.15789794922, 20.919729232788, 4373.3002929688),
+		D = CFrame.new( -1181.3093261719, 4.7514905929565, 3803.5456542969),
+		E = CFrame.new( -1612.7957763672, 36.852081298828, 149.12843322754),
+		F = CFrame.new( -690.33081054688, 15.09425163269, 1582.2380371094),
+		G = CFrame.new( -4607.82275, 872.54248, -1667.55688),
+		H = CFrame.new( -7952.31006, 5545.52832, -320.704956),
+		I = CFrame.new( -4914.8212890625, 50.963626861572, 4281.0278320313),
+		J = CFrame.new( -1427.6203613281, 7.2881078720093, -2792.7722167969),
+		K = CFrame.new(1347.8067626953, 104.66806030273, -1319.7370605469),
+		l = CFrame.new(5127.1284179688, 59.501365661621, 4105.4458007813),
+		M = CFrame.new(61163.8515625, 11.6796875, 1819.7841796875),
+		N = CFrame.new( -5247.7163085938, 12.883934020996, 8504.96875),
+		O = CFrame.new(4875.330078125, 5.6519818305969, 734.85021972656),
+		P = CFrame.new( -4813.0249, 903.708557, -1912.69055),
+		Q = CFrame.new( -4970.21875, 717.707275, -2622.35449)
+	};
+end
+local v36 = game.Players;
+local v37 = v36.LocalPlayer;
+local v38 = game:GetService("RunService");
+local v39 = v38.Heartbeat;
+local v40 = v38.RenderStepped;
+local v41 = {};
+getgenv().weapon = nil;
+getgenv().targ = nil;
+getgenv().lasttarrget = nil;
+getgenv().checked = {};
+getgenv().pl = v36:GetPlayers();
+wait(1);
+function bypass(v314)
+	if (v37.Character:FindFirstChild("Head") and v37.Character:FindFirstChild("HumanoidRootPart") and v37.Character:FindFirstChild("Humanoid")) then
+		dist = math.huge;
+		is = nil;
+		for v1638, v1639 in pairs(island) do
+			if ((v314.Position - v1639.Position).magnitude < dist) then
+				is = v1639;
+				dist = (v314.Position - v1639.Position).magnitude;
+			end
+		end
+		if (is == nil) then
+			return;
+		end
+		if (v37:DistanceFromCharacter(v314.Position) > distbyp) then
+			if ((v37.Character.Head.Position - v314.Position).magnitude > (is.Position - v314.Position).magnitude) then
+				if tween then
+					tween:Destroy();
+				end
+				if (((is.X == 61163.8515625) and (is.Y == 11.6796875) and (is.Z == 1819.7841796875)) or (is == CFrame.new( -12471.169921875 + 50 , 374.94024658203, -7551.677734375)) or (is == CFrame.new( -5085.23681640625 + 50 , 316.5072021484375, -3156.202880859375)) or (is == CFrame.new(5749.7861328125 + 50 , 611.9736938476562, -276.2497863769531))) then
+					if tween then
+						tween:Cancel();
+					end
+					repeat
+						task.wait();
+						v37.Character.HumanoidRootPart.CFrame = is;
+					until v37.Character.PrimaryPart.CFrame == is
+					task.wait(0.1);
+					game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("SetSpawnPoint");
+				elseif  not stopbypass then
+					if tween then
+						tween:Cancel();
+					end
+					repeat
+						task.wait();
+						v37.Character.HumanoidRootPart.CFrame = is;
+					until v37.Character.PrimaryPart.CFrame == is
+					game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid"):ChangeState(15);
+					v37.Character:SetPrimaryPartCFrame(is);
+					wait(0.1);
+					v37.Character.Head:Destroy();
+					wait(0.5);
+					repeat
+						task.wait();
+						v37.Character.PrimaryPart.CFrame = is;
+					until v37.Character:FindFirstChild("Humanoid").Health > 0
+					task.wait(0.5);
+				end
+			end
+		end
 	end
-    local tween_s = game:service"TweenService"
-	local info = TweenInfo.new((RealTarget.Position - game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position).Magnitude/Speed, Enum.EasingStyle.Linear)
-	local tweenw, err = pcall(function()
-		tween = tween_s:Create(game.Players.LocalPlayer.Character["HumanoidRootPart"], info, {CFrame = RealTarget})
-		tween:Play()
-	end)
+end
+local v47 = false;
+function WaitHRP(v315)
+	if  not v315 then
+		return;
+	end
+	return v315.Character:WaitForChild("HumanoidRootPart", 9);
+end
+function CheckNearestTeleporter(v316)
+	local v317 = v316.Position;
+	local v318 = math.huge;
+	local v319 = nil;
+	local v320 = game.PlaceId;
+	local v321 = {};
+	if (v320 == 2753915549) then
+		v321 = {
+			Sky3 = Vector3.new( -7894, 5547, -380),
+			Sky3Exit = Vector3.new( -4607, 874, -1667),
+			UnderWater = Vector3.new(61163, 11, 1819),
+			UnderwaterExit = Vector3.new(4050, -1, -1814)
+		};
+	elseif (v320 == 4442272183) then
+		v321 = {
+			["Swan Mansion"] = Vector3.new( -390, 332, 673),
+			["Swan Room"] = Vector3.new(2285, 15, 905),
+			["Cursed Ship"] = Vector3.new(923, 126, 32852),
+			["Zombie Island"] = Vector3.new( -6509, 83, -133)
+		};
+	elseif (v320 == 7449423635) then
+		v321 = {
+			["Floating Turtle"] = Vector3.new( -12462, 375, -7552),
+			["Hydra Island"] = Vector3.new(5745, 610, -267),
+			Mansion = Vector3.new( -12462, 375, -7552),
+			Castle = Vector3.new( -5036, 315, -3179),
+			["Beautiful Pirate"] = Vector3.new(5319, 23, -93),
+			["Beautiful Room"] = Vector3.new(5314.58203, 22.5364361, -125.942276, 1, 2.1476277e-8, -1.9911115e-13, -2.1476277e-8, 1, -3.0510602e-8, 1.984559e-13, 3.0510602e-8, 1),
+			["Temple of Time"] = Vector3.new(28286, 14897, 103)
+		};
+	end
+	for v764, v765 in pairs(v321) do
+		local v766 = (v765 - v317).Magnitude;
+		if (v766 < v318) then
+			v318 = v766;
+			v319 = v765;
+		end
+	end
+	local v322 = game.Players.LocalPlayer.Character.HumanoidRootPart.Position;
+	if (v318 <= (v317 - v322).Magnitude) then
+		return v319;
+	end
+end
+function requestEntrance(v323)
+	game.ReplicatedStorage.Remotes.CommF_:InvokeServer("requestEntrance", v323);
+	local v324 = game.Players.LocalPlayer.Character.HumanoidRootPart;
+	v324.CFrame = v324.CFrame + Vector3.new(0, 50, 0) ;
+	task.wait(0.5);
+end
+local v48 = false;
+getgenv().TweenSpeed = 350
+function TP(v326)
+	local v327 = game.Players.LocalPlayer;
+	if (v327.Character and (v327.Character.Humanoid.Health > 0) and v327.Character:FindFirstChild("HumanoidRootPart")) then
+		if  not v326 then
+			return;
+		end
+		local v1379 = (v326.Position - v327.Character.HumanoidRootPart.Position).Magnitude;
+		local v1380 = CheckNearestTeleporter(v326);
+		if v1380 then
+			requestEntrance(v1380);
+		end
+		if  not v327.Character:FindFirstChild("PartTele") then
+			local v1714 = Instance.new("Part", v327.Character);
+			v1714.Size = Vector3.new(10, 1, 10);
+			v1714.Name = "PartTele";
+			v1714.Anchored = true;
+			v1714.Transparency = 1;
+			v1714.CanCollide = true;
+			v1714.CFrame = WaitHRP(v327).CFrame;
+			v1714:GetPropertyChangedSignal("CFrame"):Connect(function()
+				if  not v47 then
+					return;
+				end
+				task.wait();
+				if (v327.Character and v327.Character:FindFirstChild("HumanoidRootPart")) then
+					local v2051 = v1714.CFrame;
+					WaitHRP(v327).CFrame = CFrame.new(v2051.Position.X, v326.Position.Y, v2051.Position.Z);
+				end
+			end);
+		end
+		if (v48 and (v1379 > distbyp)) then
+			bypass(v326);
+		end
+		v47 = true;
+		local v1381 = CFrame.new(v326.Position.X, v326.Position.Y, v326.Position.Z);
+		local v1382 = game:GetService("TweenService"):Create(v327.Character.PartTele, TweenInfo.new(v1379 / getgenv().TweenSpeed , Enum.EasingStyle.Linear), {
+			CFrame = v326
+		});
+		v1382:Play();
+		v1382.Completed:Connect(function(v1640)
+			if (v1640 == Enum.PlaybackState.Completed) then
+				if v327.Character:FindFirstChild("PartTele") then
+					v327.Character.PartTele:Destroy();
+				end
+				v47 = false;
+			end
+		end);
+	end
+end
 
 	function tweenfunc:Stop()
 		tween:Cancel()
