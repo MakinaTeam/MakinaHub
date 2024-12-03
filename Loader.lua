bac6ac6d6a5280cebc6a0f0968b6a1c881b1e2a6
MakinaTeam <158822378+MakinaTeam@users.noreply.github.com>
2024-12-03 14:35:07 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2099,28 +2099,6 @@ local function toTarget(Tween_Pos)
   end
 end
 
-local function Tween(Tween_Pos)
-  TeleportPos = Tween_Pos.p
-  local plrPP = Player.Character and Player.Character.PrimaryPart
-  if not plrPP then return end
-  local Distance = (plrPP.Position - Tween_Pos.p).Magnitude
-  local PortalPos = GetTPPos(Tween_Pos.p)
-  if (plrPP.Position - Tween_Pos.p).Magnitude > (Tween_Pos.p - PortalPos).Magnitude + 250 then
-    plrPP.CFrame = CFrame.new(PortalPos)
-    block.CFrame = CFrame.new(PortalPos)
-  elseif block then
-    if Distance <= 450 then
-      local tween = game:GetService("TweenService"):Create(block,
-      TweenInfo.new(Distance / tonumber(getgenv().TweenSpeed * 1.8), Enum.EasingStyle.Linear),
-      {CFrame = Tween_Pos}):Play()
-    else
-      local tween = game:GetService("TweenService"):Create(block,
-      TweenInfo.new(Distance / getgenv().TweenSpeed, Enum.EasingStyle.Linear),
-      {CFrame = Tween_Pos}):Play()
-    end
-  end
-end
-
 local function stopTween()
     if currentTween then
         currentTween:Cancel()
