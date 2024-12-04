9174e9e1bde6d4eceda98a26f208852a50f9fdfe
MakinaTeam <158822378+MakinaTeam@users.noreply.github.com>
2024-12-04 09:16:41 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1165,29 +1165,21 @@ local function TP(Tween_Pos)
     if not plrPP then return end
     local Distance = (plrPP.Position - Tween_Pos.p).Magnitude
     local PortalPos = GetTPPos(Tween_Pos.p)
-    if Tween_Pos.p.Y < plrPP.Position.Y then
-        plrPP.CFrame = CFrame.new(plrPP.Position.X, Tween_Pos.p.Y, plrPP.Position.Z)
-    elseif Tween_Pos.p.Y > plrPP.Position.Y then
-        plrPP.CFrame = CFrame.new(plrPP.Position.X, Tween_Pos.p.Y, plrPP.Position.Z)
-    end
-    if Distance > (Tween_Pos.p - PortalPos).Magnitude + 250 then
-        plrPP.CFrame = CFrame.new(PortalPos)
-        block.CFrame = CFrame.new(PortalPos)
-        task.wait(2) 
+    if (plrPP.Position - Tween_Pos.p).Magnitude > (Tween_Pos.p - PortalPos).Magnitude + 250 then
+      plrPP.CFrame = CFrame.new(PortalPos)
+      block.CFrame = CFrame.new(PortalPos)
     elseif block then
-        local tweenTime = Distance / getgenv().TweenSpeed
-        if Distance <= 250 then
-            tweenTime = Distance / tonumber(getgenv().TweenSpeed * 1.8)
-        end
-        if currentTween then
-            currentTween:Pause()
-        end
-        local tweenInfo = TweenInfo.new(tweenTime, Enum.EasingStyle.Linear)
-        local tweenGoal = {CFrame = Tween_Pos}
-        currentTween = TweenService:Create(block, tweenInfo, tweenGoal)
-        currentTween:Play()
+      if Distance <= 450 then
+        local tween = game:GetService("TweenService"):Create(block,
+        TweenInfo.new(Distance / tonumber(getgenv().TweenSpeed * 1.8), Enum.EasingStyle.Linear),
+        {CFrame = Tween_Pos}):Play()
+      else
+        local tween = game:GetService("TweenService"):Create(block,
+        TweenInfo.new(Distance / getgenv().TweenSpeed, Enum.EasingStyle.Linear),
+        {CFrame = Tween_Pos}):Play()
+      end
     end
-end
+  end
 
 local function stopTween()
     if currentTween then
